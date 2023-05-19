420e47bfa8a67a78a05b67445b2bf07f70ba1bab
Juan Pablo Sumski <juanpablosumski@hotmail.com>
2023-05-19 19:29:12 -0300

    feat/navbar
    Co-Authored-By: Emanuel Tiradani <28826550+EmaTiradani@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/prueba" element={<Link to="/">VOLVER</Link>} />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,15 @@
 import logo from "../assets/logo.svg";
-import "../App.css";
+import "../Home.css";
 import DarkModeButton from "../components/DarkModeButton";
 import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div className="App">
-      <header className="App-header transition-colors bg-slate-500 dark:bg-black">
+      <header className="App-header transition-colors bg-white dark:bg-slate-800">
         <img src={logo} className="App-logo" alt="logo" />
         <DarkModeButton />
-        <p>
+        <p className="text-black dark:text-white">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <Link className="App-link" to="/prueba">
