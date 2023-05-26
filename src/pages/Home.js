38f5252b87b002b913357b31c7ca90f86abf825c
Emanuel Tiradani <ematiradani@gmail.com>
2023-05-26 17:59:32 -0300

    update/home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import "../Home.css";
 import { Link } from "react-router-dom";
 import CenteredContent from "../layouts/CenteredContent";
 import Carousel from "react-bootstrap/Carousel";
+import Box from "../layouts/Box";
 
 function Home() {
   return (
@@ -47,30 +48,163 @@ function Home() {
         </Carousel.Item>
       </Carousel>
 
-      <div className="grid grid-cols-1  lg:grid-cols-3  gap-4 place-items-center  m-auto justify-center col-span-full row-span-2 row-end-4 self-center z-10 ">
-        <div class="max-w-sm rounded overflow-hidden shadow-lg bg-neutral-50">
+      <div className="grid grid-cols-1  lg:grid-cols-3  gap-3 place-items-center  m-auto justify-center col-span-full row-span-2 row-end-4 self-center z-10 ">
+        <div class="max-w-sm  h-full rounded overflow-hidden shadow-lg bg-neutral-50">
           <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-            <p class="text-gray-700 text-base">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit..
+            <div class="font-bold text-xl mb-2 text-center">
+              TODAS LAS TARJETAS
+            </div>
+            <p class="text-gray-700 text-base text-center">
+              Abona con todos los medios de pago. Mercado Pago con dinero
+              disponible o tarjeta. ¡Y siempre es seguro!
             </p>
           </div>
         </div>
-        <div class="max-w-sm rounded overflow-hidden shadow-lg bg-neutral-50">
+        <div class="flex flex-row max-w-sm rounded items-stretch h-full overflow-hidden shadow-lg bg-neutral-50 content-evenly">
           <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-            <p class="text-gray-700 text-base">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit..
+            <div class="font-bold text-xl mb-2 text-center">
+              ENVÍO RÁPIDO Y SEGURO
+            </div>
+            <p class="text-gray-700 text-base text-center">
+              Elegí recibir tu compra en tu domicilio y hacé el seguimiento
+              hasta que llegue a tus manos.
             </p>
           </div>
         </div>
-        <div class="max-w-sm rounded overflow-hidden shadow-lg bg-neutral-50">
+        <div class="max-w-sm rounded h-full overflow-hidden shadow-lg bg-neutral-50">
           <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-            <p class="text-gray-700 text-base">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit..
+            <div class="font-bold text-xl mb-2 text-center">
+              COMPRA PROTEGIDA
+            </div>
+            <p class="text-gray-700 text-base text-center">
+              Te acompañamos hasta que recibas lo que compraste. Y si no es lo
+              que esperabas, te devolvemos el dinero.
             </p>
           </div>
+        </div>
+      </div>
+
+      <div className="grid grid-rows-2 grid-cols-2 gap-1 place-items-center ">
+        <div class="max-w-sm p-6 bg-white border border-gray-200  shadow dark:bg-gray-800 dark:border-gray-700">
+          <svg
+            class="w-10 h-10 mb-2 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M5 5a3 3 0 015-2.236A3 3 0 0114.83 6H16a2 2 0 110 4h-5V9a1 1 0 10-2 0v1H4a2 2 0 110-4h1.17C5.06 5.687 5 5.35 5 5zm4 1V5a1 1 0 10-1 1h1zm3 0a1 1 0 10-1-1v1h1z"
+              clip-rule="evenodd"
+            ></path>
+            <path d="M9 11H3v5a2 2 0 002 2h4v-7zM11 18h4a2 2 0 002-2v-5h-6v7z"></path>
+          </svg>
+          <a href="#">
+            <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+              Need a help in Claim?
+            </h5>
+          </a>
+          <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+            Go to this step by step guideline process on how to certify for your
+            weekly benefits:
+          </p>
+          <a
+            href="#"
+            class="inline-flex items-center text-blue-600 hover:underline"
+          >
+            See our guideline
+            <svg
+              class="w-5 h-5 ml-2"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path>
+              <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path>
+            </svg>
+          </a>
+        </div>
+
+        <div class="max-w-sm p-6 bg-white border border-gray-200  shadow dark:bg-gray-800 dark:border-gray-700">
+          <svg
+            class="w-10 h-10 mb-2 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M5 5a3 3 0 015-2.236A3 3 0 0114.83 6H16a2 2 0 110 4h-5V9a1 1 0 10-2 0v1H4a2 2 0 110-4h1.17C5.06 5.687 5 5.35 5 5zm4 1V5a1 1 0 10-1 1h1zm3 0a1 1 0 10-1-1v1h1z"
+              clip-rule="evenodd"
+            ></path>
+            <path d="M9 11H3v5a2 2 0 002 2h4v-7zM11 18h4a2 2 0 002-2v-5h-6v7z"></path>
+          </svg>
+          <a href="#">
+            <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+              Need a help in Claim?
+            </h5>
+          </a>
+          <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+            Go to this step by step guideline process on how to certify for your
+            weekly benefits:
+          </p>
+          <a
+            href="#"
+            class="inline-flex items-center text-blue-600 hover:underline"
+          >
+            See our guideline
+            <svg
+              class="w-5 h-5 ml-2"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path>
+              <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path>
+            </svg>
+          </a>
+        </div>
+        <div class="max-w-sm p-6 bg-white border border-gray-200  shadow dark:bg-gray-800 dark:border-gray-700">
+          <svg
+            class="w-10 h-10 mb-2 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M5 5a3 3 0 015-2.236A3 3 0 0114.83 6H16a2 2 0 110 4h-5V9a1 1 0 10-2 0v1H4a2 2 0 110-4h1.17C5.06 5.687 5 5.35 5 5zm4 1V5a1 1 0 10-1 1h1zm3 0a1 1 0 10-1-1v1h1z"
+              clip-rule="evenodd"
+            ></path>
+            <path d="M9 11H3v5a2 2 0 002 2h4v-7zM11 18h4a2 2 0 002-2v-5h-6v7z"></path>
+          </svg>
+          <a href="#">
+            <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+              Need a help in Claim?
+            </h5>
+          </a>
+          <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+            Go to this step by step guideline process on how to certify for your
+            weekly benefits:
+          </p>
+          <a
+            href="#"
+            class="inline-flex items-center text-blue-600 hover:underline"
+          >
+            See our guideline
+            <svg
+              class="w-5 h-5 ml-2"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path>
+              <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path>
+            </svg>
+          </a>
         </div>
       </div>
     </div>
