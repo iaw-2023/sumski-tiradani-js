7389830b4cf653933119c9e9099a08a768c9261c
Juan Pablo Sumski <juanpablosumski@hotmail.com>
2023-06-24 21:31:19 -0300

    fix/geolocation completes names

--- src/components/comprar/Entrega.js
+++ src/components/comprar/Entrega.js
@@ -80,6 +80,9 @@ function Entrega({ compraHook, previousStep, nextStep }) {
           return response.json();
         })
         .then((data) => {
+          setCalle(data.results[0].street);
+          setNumero(data.results[0].housenumber);
+          setCiudad(data.results[0].city);
           setLatitude(data.results[0].lat);
           setLongitude(data.results[0].lon);
         })
